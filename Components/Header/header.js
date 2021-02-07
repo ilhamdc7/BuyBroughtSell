937cba28bb76c0f8600ddbbf2b58d998390a87fc
ilhamdc7 <77851709+ilhamdc7@users.noreply.github.com>
2021-02-08 00:57:34 +0400

    ...

--- Components/Header/header.js
+++ Components/Header/header.js
@@ -1,27 +1,173 @@
-import React from 'react';
-import styles from './header.module.css'
-import Message from '../../public/Media/Icons/Message.svg'
-import SearchBar from "./SearchBar";
+import React, { useState } from "react";
+import styles from "./header.module.css";
+import Message from "../../public/Media/Icons/Message.svg";
+import SearchIcon from "../../public/Media/Icons/cil_search.svg";
+import InputVoice from "../../public/Media/Icons/InputVoice.svg";
+import Basket from "../../public/Media/Icons/Basket.svg";
+import Dollar from "../../public/Media/Icons/$.svg";
+import Vector from "../../public/Media/Icons/Vector.svg";
+import Usa from "../../public/Media/Icons/usa.svg";
+import Aze from "../../public/Media/Icons/aze.svg";
+import Rus from "../../public/Media/Icons/rus.svg";
+import Spain from "../../public/Media/Icons/spain.svg";
+import Turkey from "../../public/Media/Icons/tr.svg";
+import India from "../../public/Media/Icons/india.svg";
+import China from "../../public/Media/Icons/china.svg";
+import Poland from "../../public/Media/Icons/pln.svg";
+import SubHeader from "../SubHeader/SubHeader";
 
+function Header() {
+  const [showlng, setShowlng] = useState("false");
 
+  function toggle() {
+    setShowlng(!showlng);
+  }
 
-function Header(props) {
-    return (
-        <div className={styles.header}>
-            <div className={styles.message}>
-                <Message/>
+  function closeshowlng() {
+    setShowlng(false);
+  }
 
-            </div>
+  function closeccurency() {
+    setShowcurrency(false);
+  }
 
-            <div className={styles.srcbar}>
-                <SearchBar/>
-            </div>
+  function allInOne() {
+    closeshowlng();
+    ccurrency();
+  }
 
-            
+  function allInOne2() {
+    closeccurency();
+    toggle();
+  }
 
+  const [showcurrency, setShowcurrency] = useState("false");
+
+  function ccurrency() {
+    setShowcurrency(!showcurrency);
+  }
+
+  return (
+    <div className={styles.header}>
+      <div className={styles.search}>
+        <input
+          className={styles.int}
+          placeholder={"   All you need are here..."}
+          type={"text"}
+        />
+
+        <div className={styles.SearchIcon}>
+          <SearchIcon />
         </div>
+      </div>
 
-    );
+      <div className={styles.inptvoice}>
+        <InputVoice />
+      </div>
+      <div className={styles.basket}>
+        <Basket />
+      </div>
+      <a className={styles.sign} href="#">
+        Sign In
+      </a>
+
+      <div
+        className={styles.showcurrency}
+        showcurrency={showcurrency}
+        style={{ display: showcurrency ? "block" : "none" }}
+      >
+        <ul>
+          <li>
+            <Usa />
+            USD
+          </li>
+          <li>
+            <Aze />
+            AZN
+          </li>
+          <li>
+            <Rus />
+            RUBL
+          </li>
+          <li>
+            <Spain />
+            EURO
+          </li>
+          <li>
+            <Turkey />
+            TL
+          </li>
+          <li>
+            <India />
+            INR
+          </li>
+          <li>
+            <China />
+            CYN
+          </li>
+          <li>
+            <Poland />
+            PLN
+          </li>
+        </ul>
+      </div>
+
+      <div className={styles.currency} onClick={allInOne}>
+        <Dollar />
+        <div className={styles.vectorcurrency} onClick={allInOne}>
+          <Vector />
+        </div>
+      </div>
+
+      <div className={styles.language} onClick={allInOne2}>
+        <ul>
+          <li>En</li>
+        </ul>
+
+        <div
+          className={styles.showlanguage}
+          show={showlng}
+          style={{ display: showlng ? "block" : "none" }}
+        >
+          <ul>
+            <li>
+              <a href="#">En</a>
+            </li>
+            <li>
+              <a href="#">Ru</a>
+            </li>
+            <li>
+              <a href="#">AZ</a>
+            </li>
+            <li>
+              <a href="#">TR</a>
+            </li>
+            <li>
+              <a href="#">ES</a>
+            </li>
+            <li>
+              <a href="#">Pl</a>
+            </li>
+            <li>
+              <a href="#">IN</a>
+            </li>
+            <li>
+              <a href="#">ZH</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className={styles.vectorlanguage}>
+        <Vector onClick={allInOne2} />
+      </div>
+
+      <div className={styles.message}>
+        <Message />
+      </div>
+
+      <SubHeader className={styles.Subheader} />
+    </div>
+  );
 }
 
 export default Header;
